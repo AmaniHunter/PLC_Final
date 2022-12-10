3642e152144b46a13016ac41a26fcc43a063e943
Amani Hunter <amanikhunter@gmail.com>
2022-12-09 20:20:05 -0500

    Added Compiler class with requirements.

--- Compiler.py
+++ Compiler.py
@@ -1,2 +1,18 @@
+from Parser import Parser
+from Lexer import Lexer
+
+
 class Compiler:
-    
+	def __init__(self, file):
+		# takes in an INPUT FILE and converts it to one input str
+		self.code = file.read()
+		lexer = Lexer(self.code)
+		if lexer.tokenizer():
+			tokens = lexer.tokens
+			#print('Tokens: ',tokens)
+			if len(tokens) > 0:
+				parser = Parser(tokens)
+				parser.beginParsing(parser.parseTree)
+				parser.parseTree.printParseTree()
+		else:
+			print("Unrecognized Lexeme at index {:2d} is invalid\n".format(len(lexer.tokens)))
